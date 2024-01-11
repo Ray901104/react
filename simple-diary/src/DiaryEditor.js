087137d8f325b393eabf4823317f1f0cdadaf524
yrcho <ip323@naver.com>
2024-01-11 18:20:40 +0900

    React에서 DOM 조작하기 - useRef

--- simple-diary/src/DiaryEditor.js
+++ simple-diary/src/DiaryEditor.js
@@ -1,6 +1,9 @@
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 const DiaryEditor = () => {
+    const authorInput = useRef();
+    const contentTextarea = useRef();
+
     const [state, setState] = useState({
         author: "",
         content: "",
@@ -15,7 +18,15 @@ const DiaryEditor = () => {
     };
 
     const handleSubmit = () => {
-        console.log(state);
+        if (state.author.length < 1) {
+            authorInput.current.focus();
+            return;
+        }
+        if (state.content.length < 5) {
+            contentTextarea.current.focus();
+            return;
+        }
+
         alert("저장 완료");
     }
 
@@ -23,19 +34,30 @@ const DiaryEditor = () => {
         <div className="DiaryEditor">
             <h2>오늘의 일기</h2>
             <div>
-                <input name="author" value={state.author} onChange={handleChangeState}/>
+                <input
+                    ref={authorInput}
+                    name="author"
+                    value={state.author}
+                    onChange={handleChangeState}/>
             </div>
             <div>
-                <textarea name="content" value={state.content} onChange={handleChangeState}/>
+                <textarea
+                    ref={contentTextarea}
+                    name="content"
+                    value={state.content}
+                    onChange={handleChangeState}/>
                 />
             </div>
             <div>
-                <select name="emotion" value={state.emotion} onChange={handleChangeState}>
-                    <option value={1}>1</option>
-                    <option value={2}>2</option>
-                    <option value={3}>3</option>
-                    <option value={4}>4</option>
-                    <option value={5}>5</option>
+                <select
+                    name="emotion"
+                    value={state.emotion}
+                    onChange={handleChangeState}>
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
+                        <option value={5}>5</option>
                 </select>
             </div>
             <div>
