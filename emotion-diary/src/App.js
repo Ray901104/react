a011b59d8992c891273f84c8116308b801fcd047
yrcho <ip323@naver.com>
2024-01-17 21:26:27 +0900

    프로젝트 기초 공사 - 공용 헤더 및 버튼 작성

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -5,11 +5,21 @@ import Home from "./pages/Home";
 import Edit from "./pages/Edit";
 import New from "./pages/New";
 
+//components
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
+
 function App() {
   return (
       <BrowserRouter>
         <div className="App">
-          <h2>App.js</h2>
+          <MyHeader headText={"App"}
+                    leftChild={<MyButton text={"왼쪽 버튼"} onClick={() => {alert("왼쪽 클릭")}}/>}
+                    rightChild={<MyButton text={"오른쪽 버튼"} onClick={() => {alert("오른쪽 클릭")}} />}/>
+            <h2>App.js</h2>
+            <MyButton text={"버튼"} onClick={() => {alert("버튼 클릭")}} type={"positive"}/>
+            <MyButton text={"버튼"} onClick={() => {alert("버튼 클릭")}} type={"negative"}/>
+            <MyButton text={"버튼"} onClick={() => {alert("버튼 클릭")}} />
             <Routes>
                 <Route path="/" element={<Home />}/>
                 <Route path="/diary/:id" element={<Diary />}/>
