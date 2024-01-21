6a39c0f8f19d20af90f7d8d38fb8f33cfa2442e5
yrcho <ip323@naver.com>
2024-01-21 17:08:48 +0900

    페이지 구현 - 홈 (/)

--- emotion-diary/src/pages/Home.js
+++ emotion-diary/src/pages/Home.js
@@ -1,6 +1,55 @@
+import {useContext, useEffect, useState} from "react";
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+import {DiaryStateContext} from "../App";
+import DiaryList from "../components/DiaryList"
+
 const Home = () => {
+    const [currDate, setCurrDate] = useState(new Date());
+    const headText = `${currDate.getFullYear()}년 ${currDate.getMonth() + 1}월`;
+
+    const diaryList = useContext(DiaryStateContext);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        if (diaryList.length >= 1) {
+            const firstDay = new Date(
+                currDate.getFullYear(),
+                currDate.getMonth(),
+                1,
+            ).getTime();
+
+            const lastDay = new Date(
+                currDate.getFullYear(),
+                currDate.getMonth() + 1,
+                0
+            ).getTime();
+
+            setData(diaryList.filter((item) => item.date >= firstDay && item.date <= lastDay));
+        }
+    }, [diaryList, currDate]);
+
+    const increaseMonth = () => {
+        setCurrDate(
+            new Date(currDate.getFullYear(), currDate.getMonth() + 1, currDate.getDate())
+        );
+    }
+
+    const decreaseMonth = () => {
+        setCurrDate(
+            new Date(currDate.getFullYear(), currDate.getMonth() - 1, currDate.getDate())
+        );
+    }
+
     return (
-        <div>일기 메인 페이지입니다.</div>
+        <div>
+            <MyHeader
+                headText={headText}
+                leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+                rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+            />
+            <DiaryList diaryList={data}/>
+        </div>
     )
 }
 
